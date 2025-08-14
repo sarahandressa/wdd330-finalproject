dbcfd637044bd0c0e56562f1bad4fc7161d8b326
sarahandressa <sarahagdiaz@yahoo.com.br>
2025-08-14 01:59:30 -0300

    update Week 06 - final project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
-export default {
-  root: 'public',
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  root: 'public', 
   build: {
-    outDir: '../dist',
+    outDir: '../dist', 
     emptyOutDir: true,
+    rollupOptions: {
+      
+      input: {
+        index: resolve(__dirname, 'public/index.html'),
+        book: resolve(__dirname, 'public/book.html'),
+        club: resolve(__dirname, 'public/club.html'),
+      },
+      output: {
+        
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   },
   server: {
     port: 3000,
   },
-};
+})
